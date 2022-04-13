cccc35707b7796e4647683e9666a433442fb3aaa
sepideh <sepideh.ayani@target.com>
2022-04-13 11:46:20 -0500

    cleanups

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -1,29 +1,10 @@
 import React from "react";
 import {
-    CFooter,
-    CLink
+    CFooter
 } from '@coreui/react';
 
 function Footer() {
     return (
-        // <CFooter>
-        //     <div className="image" name="Links">
-        //         <a href="https://github.com/SepidehAyani">
-        //             <img alt="GitHub" src="../public/icons/github_logo.png" /></a>
-        //         <a href="https://www.linkedin.com/in/sepidehayani/">
-        //             <img alt="LinkedIn" src="./icons/linkedin_logo.png" /></a>
-        //         <a href="https://www.linkedin.com/in/sepidehayani/">
-        //         <img alt="Twitter" src="./icons/twitter_logo.png" /></a>
-        //     </div>
-        //     <div>
-        //         <CLink href="https://coreui.io">CoreUI</CLink>
-        //         <span>&copy; 2022 creativeLabs.</span>
-        //     </div>
-        //     <div>
-        //         <span>Powered by </span>
-        //         <CLink href="https://coreui.io">CoreUI</CLink>
-        //     </div>
-        // </CFooter>
         <CFooter>
             <div className="image" name="Links">
                 <a href="https://github.com/SepidehAyani"><img src="./icons/github_logo.png" alt="GitHub"  width="42" height="42" /></a>
--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,9 +1,10 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from "react";
 
 function Header() {
     return (
         <div >
-            <div class="avatar avatar-lg bg-primary text-white">SA</div>
+            <div class="avatar avatar-lg bg-primary text-white" style={{ marginTop: "3px" }}>SA</div>
         </div>
     );
 }
